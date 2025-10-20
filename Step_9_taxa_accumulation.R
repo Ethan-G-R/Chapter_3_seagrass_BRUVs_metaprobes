8f0a77c2db207ad8320a5df47a0c8d3b345e574f
Ross <r01er21@abdn.ac.uk>
2025-10-20 09:46:41 +0100

    Updated sp accumulation curves using iNEXT

--- Step_9_taxa_accumulation.R
+++ Step_9_taxa_accumulation.R
@@ -150,6 +150,316 @@ accum_sp
 ggsave(filename = "plots/accum/accum_sp_col.png", plot = accum_sp,
        device = "png", dpi = 600, units = "mm", height = 150, width = 200)
 
+################################################################################
+############################ with iNEXT ########################################
+# 1. Install the iNEXT package
+#install.packages("iNEXT")
+
+library(iNEXT)
+
+######## Get my data
+
+load("working_phyloseq_data/met_bruv_pseq_glom_sp.RData")
+
+tax_table(met_bruv_pseq_sp)
+otu_table(met_bruv_pseq_sp)
+sample_data(met_bruv_pseq_sp)
+
+meta_data <- data.frame(t(otu_table(met_bruv_pseq_sp)))
+
+meta_data <- meta_data %>%
+  rownames_to_column(var = "Sample")
+
+################################################################################
+
+meta_sample_data <- data.frame(sample_data(met_bruv_pseq_sp))
+
+meta_sample_data <- meta_sample_data %>%
+  select(deployment, SampleType)
+
+meta_sample_data <- meta_sample_data %>%
+  rownames_to_column(var = "Sample")
+
+
+#################### Combine ###################################################
+
+meta_data_full <- full_join(meta_sample_data, meta_data, by = "Sample")
+
+##### Species accumulation curves #####
+# Function to convert a column to binary based on a threshold
+binary_converter <- function(column, threshold) {
+  binary_data <- ifelse(column >= threshold, 1, 0)
+  return(binary_data)
+}
+
+str(meta_data_full)
+
+meta_data_full_deployment <- meta_data_full %>%
+  filter(SampleType == "Metaprobe") %>%
+  select(-SampleType)
+
+str(meta_data_full_deployment)
+
+# Apply the binary_converter function to all columns
+AccCurve_12S <- meta_data_full_deployment[,c(3:28)]
+thresholds <- 1
+AccCurve_12S <- data.frame(lapply(AccCurve_12S, binary_converter, threshold = thresholds))
+AccCurve_12S <- data.frame(meta_data_full_deployment[,c(1,2)], AccCurve_12S)
+
+## Accumulation curves 12S (iNEXT format)
+# the second column (deployment) is removed since all values are the same after
+# which selects for specific deployment
+# Row names are then turned into a column
+
+Accdep1 <- AccCurve_12S[which(AccCurve_12S$deployment == '1'),-2]
+Accdep1 <- as.data.frame(t(Accdep1))
+colnames(Accdep1) <- Accdep1[1,]
+Accdep1 <- Accdep1[-1,]
+Accdep1$species <- rownames(Accdep1)
+rownames(Accdep1) <- NULL
+
+Accdep2 <- AccCurve_12S[which(AccCurve_12S$deployment == '2'),-2]
+Accdep2 <- as.data.frame(t(Accdep2))
+colnames(Accdep2) <- Accdep2[1,]
+Accdep2 <- Accdep2[-1,]
+Accdep2$species <- rownames(Accdep2)
+rownames(Accdep2) <- NULL
+
+Accdep3 <- AccCurve_12S[which(AccCurve_12S$deployment == '3'),-2]
+Accdep3 <- as.data.frame(t(Accdep3))
+colnames(Accdep3) <- Accdep3[1,] 
+Accdep3 <- Accdep3[-1,]
+Accdep3$species <- rownames(Accdep3)
+rownames(Accdep3) <- NULL
+
+Accdep4 <- AccCurve_12S[which(AccCurve_12S$deployment == '4'),-2]
+Accdep4 <- as.data.frame(t(Accdep4))
+colnames(Accdep4) <- Accdep4[1,]
+Accdep4 <- Accdep4[-1,]
+Accdep4$species <- rownames(Accdep4)
+rownames(Accdep4) <- NULL
+
+# Turns fourth column (species names0 back into row names and makes sure
+# the numbers are all integers
+
+dep1 <- as.matrix(apply(Accdep1[,-4],2,as.integer))
+row.names(dep1) <- Accdep1[,4]
+dep2 <- as.matrix(apply(Accdep2[,-4],2,as.integer))
+row.names(dep2) <- Accdep2[,4]
+dep3 <- as.matrix(apply(Accdep3[,-4],2,as.integer))
+row.names(dep3) <- Accdep3[,4]
+dep4 <- as.matrix(apply(Accdep4[,-4],2,as.integer))
+row.names(dep4) <- Accdep4[,4]
+
+################################################################################
+
+AccCurve12S = list(dep1 = dep1, dep2 = dep2, dep3 = dep3, dep4 = dep4)
+
+str(AccCurve12S)
+
+# End point is how many samples the accumulation curve is estimated up to
+
+Acc12S_next_met <- iNEXT(AccCurve12S, datatype="incidence_raw", endpoint=12)
+
+Acc12S_met <- ggiNEXT(Acc12S_next_met) +
+  scale_fill_manual(values = c(
+    "dep1" = "#FF654B",
+    "dep2" = "#EBCC2A",
+    "dep3" = "#38C682",
+    "dep4" = "#30788C"),
+    labels = c(
+      "dep1" = "1",
+      "dep2" = "2",
+      "dep3" = "3",
+      "dep4" = "4"
+    )) +
+  scale_color_manual(values = c(
+    "dep1" = "#FF654B",
+    "dep2" = "#EBCC2A",
+    "dep3" = "#38C682",
+    "dep4" = "#30788C"),
+    labels = c(
+      "dep1" = "1",
+      "dep2" = "2",
+      "dep3" = "3",
+      "dep4" = "4"
+    )) +
+  scale_shape_manual(values = c(
+    "dep1" = 15, # Solid circle
+    "dep2" = 16, 
+    "dep3" = 18, 
+    "dep4" = 17),
+    labels = c(
+      "dep1" = "1",
+      "dep2" = "2",
+      "dep3" = "3",
+      "dep4" = "4"
+    )) +  
+  xlab("Number of samples") + ylab("Species richness") +
+  scale_x_continuous(breaks = seq(from = 3, to = 12, by = 3)) +
+  scale_y_continuous(limits = c(2, 30)) +
+  theme_classic() + 
+  guides(
+    color = guide_legend(title = "Metaprobe deployment", order = 1),
+    fill = guide_legend(title = "Metaprobe deployment", order = 1),
+    shape = guide_legend(title = "Metaprobe deployment", order = 1),
+    # Target the guide responsible for the line/method (usually linetype)
+    linetype = guide_legend(title = NULL, order = 2) 
+  ) +
+  theme(
+    axis.title.x = element_text(size = 20),
+    axis.title.y = element_text(size = 20),
+    axis.text.x = element_text(size = 20),
+    axis.text.y = element_text(size = 20),
+    legend.text = element_text(size = 15),
+    legend.title = element_text(size = 17.5),
+    legend.title.position = "top",
+    legend.key.size = unit(1, "cm"),
+    legend.position = "bottom"
+  )
+
+
+Acc12S_met
+
+ggsave(filename = "plots/accum/Metaprobe_rare.png", plot = Acc12S_met,
+       device = "png", dpi = 600, units = "mm", height = 125, width = 150)
+
+################################################################################
+
+### Metaprobe vs BRUV
+
+meta_data_full_method <- meta_data_full %>%
+  select(-deployment)
+
+str(meta_data_full_method)
+
+# Apply the binary_converter function to all columns
+AccCurve_12S <- meta_data_full_method[,c(3:28)]
+thresholds <- 1
+AccCurve_12S <- data.frame(lapply(AccCurve_12S, binary_converter, threshold = thresholds))
+AccCurve_12S <- data.frame(meta_data_full_method[,c(1,2)], AccCurve_12S)
+
+## Accumulation curves 12S
+Accmet <- AccCurve_12S[which(AccCurve_12S$SampleType == 'Metaprobe'),-2]
+Accmet <- as.data.frame(t(Accmet))
+colnames(Accmet) <- Accmet[1,]
+Accmet <- Accmet[-1,]
+Accmet$species <- rownames(Accmet)
+rownames(Accmet) <- NULL
+
+AccBRUV <- AccCurve_12S[which(AccCurve_12S$SampleType == 'BRUV'),-2]
+AccBRUV <- as.data.frame(t(AccBRUV))
+colnames(AccBRUV) <- AccBRUV[1,]
+AccBRUV <- AccBRUV[-1,]
+AccBRUV$species <- rownames(AccBRUV)
+rownames(AccBRUV) <- NULL
+
+met <- as.matrix(apply(Accmet[,-13],2,as.integer))
+row.names(met) <- Accmet[,13]
+BRUV <- as.matrix(apply(AccBRUV[,-5],2,as.integer))
+row.names(BRUV) <- AccBRUV[,5]
+
+################################################################################
+
+AccCurve12S = list(met = met, BRUV = BRUV)
+
+str(AccCurve12S)
+
+Acc12S_next_met_vs_BRUV <- iNEXT(AccCurve12S, datatype="incidence_raw", endpoint=20)
+
+Acc12S_met_vs_BRUV <- ggiNEXT(Acc12S_next_met_vs_BRUV) +
+  scale_fill_manual(values = c(
+    "met" = "#38C682",
+    "BRUV" = "black"),
+    labels = c(
+      "met" = "Metaprobe",
+      "BRUV" = "BRUV")) +
+  scale_color_manual(values = c(
+    "met" = "#38C682",
+    "BRUV" = "black"),
+    labels = c(
+      "met" = "Metaprobe",
+      "BRUV" = "BRUV")) +
+  scale_shape_manual(values = c(
+    "met" = 16,
+    "BRUV" = 17),
+    labels = c(
+      "met" = "Metaprobe",
+      "BRUV" = "BRUV")) +
+  xlab("Number of samples") + ylab("Species richness") +
+  scale_y_continuous(limits = c(2, 30)) +
+  theme_classic() +
+  guides(
+    color = guide_legend(title = "Detection method", order = 1),
+    fill = guide_legend(title = "Detection method", order = 1),
+    shape = guide_legend(title = "Detection method", order = 1),
+    # Target the guide responsible for the line/method (usually linetype)
+    linetype = guide_legend(title = NULL, order = 2) 
+  ) +
+  theme(
+    axis.title.x = element_text(size = 20),
+    axis.title.y = element_text(size = 20),
+    axis.text.x = element_text(size = 20),
+    axis.text.y = element_text(size = 20),
+    legend.text = element_text(size = 15),
+    legend.title = element_text(size = 17.5),
+    legend.title.position = "top",
+    legend.key.size = unit(1, "cm"),
+    legend.position = "bottom"
+  ) 
+
+
+Acc12S_met_vs_BRUV
+
+ggsave(filename = "plots/accum/Metaprobe_vs_BRUV_rare.png", plot = Acc12S_met_vs_BRUV,
+       device = "png", dpi = 600, units = "mm", height = 125, width = 150)
+
+################################################################################
+
+
+AccCurve12S_all = list(met = met, BRUV = BRUV, dep1 = dep1, dep2 = dep2, dep3 = dep3, dep4 = dep4)
+
+str(AccCurve12S_all)
+
+Acc12S_next_all <- iNEXT(AccCurve12S_all, datatype="incidence_raw", endpoint=20)
+
+Acc12S_all <- ggiNEXT(Acc12S_next_all) +
+  scale_fill_manual(values = c(
+    "met" = "#7F7F7F",
+    "BRUV" = "black",
+    "dep1" = "#FF654B",
+    "dep2" = "#EBCC2A",
+    "dep3" = "#38C682",
+    "dep4" = "#30788C")) +
+  scale_color_manual(values = c(
+    "met" = "#7F7F7F",
+    "BRUV" = "black",
+    "dep1" = "#FF654B",
+    "dep2" = "#EBCC2A",
+    "dep3" = "#38C682",
+    "dep4" = "#30788C")) +
+  scale_shape_manual(values = c(
+    "met" = 16,
+    "BRUV" = 17, # Solid triangle
+    "dep1" = 16, # Solid circle
+    "dep2" = 16, 
+    "dep3" = 16, 
+    "dep4" = 16  
+  )) +
+  xlab("Number of Samples") + ylab("Species richness") +
+  theme_classic() +
+  theme(
+    axis.title.x = element_text(size = 20),
+    axis.title.y = element_text(size = 20),
+    axis.text.x = element_text(size = 20),
+    axis.text.y = element_text(size = 20),
+    legend.text = element_text(size = 20),
+    legend.title = element_text(size = 20),
+    legend.key.size = unit(1, "cm"),
+    legend.position = "bottom"
+  ) 
+
+Acc12S_all
 
 
 
